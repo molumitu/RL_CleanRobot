38e2acb7567d0ee8962ba1196cd33fb94723f77b
molumitu <zgj21@mails.tsinghua.edu.cn>
2022-03-24 14:15:35 +0800

    fix evaluation

--- mc_agent.py
+++ mc_agent.py
@@ -1,6 +1,5 @@
 import numpy as np
 from Env import GridEnv
-gamma = 1
 import copy
 
 def cumulative_return(rewards, gamma):
@@ -12,7 +11,8 @@ def cumulative_return(rewards, gamma):
     return cumulative_rewards
 
 class StateValue():
-    def __init__(self, H, W, seed=1) -> None:
+    def __init__(self, H, W, seed=0) -> None:
+        self.update_times = 0
         self.v = np.zeros((H, W))
         self.tem_v = self.v.copy()
         self.v_count = np.zeros_like(self.v, dtype=np.int64)
@@ -20,6 +20,7 @@ class StateValue():
         self.rng = np.random.default_rng(seed=seed)
 
     def update(self, obs_trajectory, reward_trajectoty):
+        self.update_times += 1
         cumulative_rewards = cumulative_return(reward_trajectoty, gamma)
         for i, obs in enumerate(obs_trajectory):
             self.v_count[obs[0], obs[1]] += 1
@@ -35,11 +36,11 @@ class StateValue():
         self.tem_v = self.v.copy()
         return done
         
-    
+gamma = 0.9
 env = GridEnv()
 v_table = StateValue(env.H, env.W)
 max_iteration = 2000
-episode_num = 100
+episode_num = 1000
 for i in range(max_iteration):
     for j in range(episode_num):
         obs_trajectory = []
@@ -57,15 +58,39 @@ for i in range(max_iteration):
                 break
             obs_trajectory.append(next_obs.copy())
             step += 1
-    v_table.render()
     if v_table.check_convergence():
-        print("R_avg=", np.sum(v_table.v)/(env.H*env.W - 1))
+        v_table.render()
+        print("R_avg =", np.sum(v_table.v)/(env.H*env.W - 1))
+        print('Update times =', v_table.update_times)
         break
-    
 
-    
-
-
+gamma = 1
+env = GridEnv()
+v_table = StateValue(env.H, env.W)
+max_iteration = 2000
+episode_num = 1000
+for i in range(max_iteration):
+    for j in range(episode_num):
+        obs_trajectory = []
+        reward_trajectoty = []
+        env.reset()
+        init_obs = env.obs.copy()
+        obs_trajectory.append(init_obs)
+        step = 0
+        while(True):
+            action = v_table.rng.integers(0, env.action_num)
+            next_obs, reward, done, info = env.step(action)
+            reward_trajectoty.append(reward)
+            if done:
+                v_table.update(obs_trajectory, reward_trajectoty)
+                break
+            obs_trajectory.append(next_obs.copy())
+            step += 1
+    if v_table.check_convergence():
+        v_table.render()
+        print("R_avg =", np.sum(v_table.v)/(env.H*env.W - 1))
+        print('Update times =', v_table.update_times)
+        break
 
 
 
--- q_learning.py
+++ q_learning.py
@@ -1,11 +1,14 @@
 
+from gym import Env
 from Env import GridEnv
 import numpy as np
 
 
 
 class ActionValue:
-    def __init__(self, H, W, action_num, seed=1) -> None:
+    def __init__(self, H, W, action_num, seed=0) -> None:
+        self.update_times = 0
+        self.rng = np.random.default_rng(seed=seed)
         self.action_num = action_num
         self.q = np.zeros((H, W, self.action_num))
         self.tem_q = self.q.copy()
@@ -13,18 +16,23 @@ class ActionValue:
         self.gamma = 0.9
 
     def get_action(self, obs):
-        return np.random.randint(4)
+        return self.rng.integers(4)
+
+    def get_action_with_policy(self, obs):
+        return np.argmax(self.q[obs[0], obs[1]])
 
     def update(self, obs, action, reward, next_obs):
+        self.update_times += 1
         self.q[obs[0], obs[1], action] = (1-self.alpha)*self.q[obs[0], obs[1], action] + self.alpha * (reward + self.gamma * np.max(self.q[next_obs[0], next_obs[1]]))
 
     def render(self):
         print(np.argmax(self.q, axis=-1))
         print(self.q.reshape(-1, self.action_num))
-        print(self.tem_q.reshape(-1, self.action_num))
+        # print(self.tem_q.reshape(-1, self.action_num))
+        print('Update times = ', self.update_times)
 
     def check_convergence(self):
-        done = np.allclose(self.q, self.tem_q, atol=0.0001)
+        done = np.allclose(self.q, self.tem_q, atol=0.00001)
         self.tem_q = self.q.copy()
         return done
 
@@ -45,5 +53,32 @@ for i in range(max_iteration):
         # print("R_avg=", np.sum(q_table.q)/(env.H*env.W - 1))
         q_table.render()
         break
+
+return_mat_total = np.zeros((4,4))
+env = GridEnv()
+eva_num = 1000
+for e in range(eva_num):
+    return_mat = np.zeros((4,4))
+    for i in range(env.H):
+        for j in range(env.W):
+            if i==env.H-1 and j==env.W-1:
+                pass
+            else:
+                obs = np.array([i,j])
+                env.set_obs(obs)
+                while True:
+                    action = q_table.get_action_with_policy(obs)
+                    next_obs, reward, done, info = env.step(action)
+                    obs = next_obs
+                    return_mat[i,j] += reward
+                    if done:
+                        break 
+    return_mat_total += return_mat
+print(return_mat_total/eva_num)
+print('Average return is :', sum(return_mat_total.flatten())/eva_num/(env.H*env.W - 1))
+
+
+        
+
 
     
